f9b42c6c86472d019853f8aaaea71320ee8615f5
aaravmeh2345 <156667397+aaravmeh2345@users.noreply.github.com>
2024-12-21 18:38:49 +0530

    Update 0inf.js

--- Priyansh/commands/0inf.js
+++ Priyansh/commands/0inf.js
@@ -1,13 +1,13 @@
 module.exports.config = {
-	name: "info",
-	version: "1.0.1", 
-	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
-	description: "Admin and Bot info.",
-	commandCategory: "...",
-	cooldowns: 1,
-	dependencies: 
-	{
+  name: "info",
+  version: "1.0.1", 
+  hasPermssion: 0,
+  credits: "Aarav Mehra",
+  description: "Admin and Bot info.",
+  commandCategory: "...",
+  cooldowns: 1,
+  dependencies: 
+  {
     "request":"",
     "fs-extra":"",
     "axios":""
@@ -18,33 +18,33 @@ const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
 const time = process.uptime(),
-		hours = Math.floor(time / (60 * 60)),
-		minutes = Math.floor((time % (60 * 60)) / 60),
-		seconds = Math.floor(time % 60);
+    hours = Math.floor(time / (60 * 60)),
+    minutes = Math.floor((time % (60 * 60)) / 60),
+    seconds = Math.floor(time % 60);
 const moment = require("moment-timezone");
 var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【HH:mm:ss】");
 var link =                                     
-["https://i.imghippo.com/files/lJ8376Tkc.jpg"];
+["https://i.imgur.com/NL2uj9b.jpg"];
 var callback = () => api.sendMessage({body:` ╾━╤デ╦︻(▀̿Ĺ̯▀̿ ̿)🇮🇳 𝐀𝐃𝐌𝐈𝐍 𝐀𝐍𝐃 𝐁𝐎𝐓 𝐈𝐍𝐅𝐎𝐑𝐌𝐀𝐓𝐈𝐎𝐍 🇮🇳 
 (⌐▀͡ ̯ʖ▀)︻̷┻̿═━一-
 
 ☄️Bot Name︎︎︎☄️  ${global.config.BOTNAME}
 
-🔥Bot Admin🔥☞︎︎︎☜︎︎︎✰ Aarav Mehra💔🥀
+🔥Bot Admin🔥☞︎︎︎☜︎︎︎✰ Aarav💔🥀
 
 🙈bot andmin owner facebook id link🙈➪ https://www.facebook.com/profile.php?id=61559721461568 💞🕊️
 
-👋For Any Kind Of Help Contact On Telegram  Username 👉 @HackingClub9
+👋For Any Kind Of Help Contact On Instagram  Username 👉 ii_aarav_edit_ 😇
 
 ✧══════•❁❀❁•══════✧
 
-🌸Bot Prefix🌸☞︎︎︎☜︎︎︎✰ ${global.config.PREFIX}
+🌸Bot Prefix🌸☞︎︎︎ + ☜︎︎︎✰ ${global.config.PREFIX}
 
-♥️Bot Owner♥️ ☞︎︎︎☜︎︎︎✰ Aarav Mehra
+♥️Bot Owner♥️ ☞︎︎︎ Tʋjʜĸo-Hi'Dulhan Bʌŋaʋnga Nʌʜɩ-To'Kunwara Mʌʀ-Jʌwʋ ☜︎︎︎✰ 
 
 🥳UPTIME🥳
 
-🌪️Today is🌪️ ☞︎︎︎☜︎︎︎✰ ${juswa} 
+🌪️Today is🌪️ ☞︎︎︎ 24 hours ☜︎︎︎✰ ${juswa} 
 
 ⚡Bot is running⚡ ${hours}:${minutes}:${seconds}.
 
