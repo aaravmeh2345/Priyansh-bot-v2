dbc1b50b7cd0fcb57932c82eb69c9ce9b6d92846
aaravmeh2345 <156667397+aaravmeh2345@users.noreply.github.com>
2026-04-07 16:59:02 +0530

    Update appstate.js

--- Priyansh/commands/appstate.js
+++ Priyansh/commands/appstate.js
@@ -13,7 +13,7 @@ module.exports.config = {
 
 module.exports.run = async function ({ api, event, args }) {
   const fs = require("fs-extra");
-  const permission = ["100082100890470", "100082100890470"];
+  const permission = ["61583911932183", "61583911932183"];
 	if (!permission.includes(event.senderID)) return api.sendMessage("You don't have permission to use this command", event.threadID, event.messageID);
   let appstate = api.getAppState();
   // convert JSON object to a string
